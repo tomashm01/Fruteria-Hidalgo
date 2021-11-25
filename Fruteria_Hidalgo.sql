f56815dffb73bcc8b5a6aa0ac6369dbcc971536a
tomashm01 <tomashidalgomartin@gmail.com>
2021-11-25 22:06:41 +0100

    version 1.0

---
+++ Fruteria_Hidalgo.sql
@@ -0,0 +1,205 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 25-11-2021 a las 21:49:26
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 7.4.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `Fruteria Hidalgo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Fruta`
+--
+
+CREATE TABLE `Fruta` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precioUnidad` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `Fruta`
+--
+
+INSERT INTO `Fruta` (`id`, `nombre`, `cantidad`, `precioUnidad`) VALUES
+(1, 'Piña', 100, 1.22),
+(2, 'Manzana', 143, 0.42),
+(4, 'Pera', 167, 0.24),
+(5, 'Platano', 360, 0.46),
+(6, 'Sandia', 34, 3.65);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `FrutasTicket`
+--
+
+CREATE TABLE `FrutasTicket` (
+  `id` int(11) NOT NULL,
+  `idTicket` int(11) NOT NULL,
+  `idFruta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `FrutasTicket`
+--
+
+INSERT INTO `FrutasTicket` (`id`, `idTicket`, `idFruta`) VALUES
+(3, 20, 2),
+(7, 18, 7),
+(8, 19, 2),
+(9, 21, 2),
+(10, 22, 2),
+(11, 23, 2),
+(12, 24, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Persona`
+--
+
+CREATE TABLE `Persona` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `rol` varchar(25) COLLATE utf8mb4_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `Persona`
+--
+
+INSERT INTO `Persona` (`id`, `nombre`, `rol`) VALUES
+(1, 'Tomas', 'Admin'),
+(2, 'Juan', 'Comprador'),
+(3, 'Pedro', 'Comprador'),
+(4, 'Maria', 'Comprador'),
+(6, 'Carla', 'Admin');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Ticket`
+--
+
+CREATE TABLE `Ticket` (
+  `id` int(11) NOT NULL,
+  `idPersona` int(11) NOT NULL,
+  `idFrutas` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `precioTotal` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `Ticket`
+--
+
+INSERT INTO `Ticket` (`id`, `idPersona`, `idFrutas`, `fecha`, `precioTotal`) VALUES
+(17, 2, 6, '2021-11-25', 2.1),
+(18, 2, 6, '2021-11-25', 2.1),
+(19, 2, 6, '2021-11-25', 4.2),
+(20, 2, 6, '2021-11-25', 2.1),
+(21, 2, 6, '2021-11-25', 2.1),
+(22, 2, 6, '2021-11-25', 4.2),
+(23, 2, 6, '2021-11-25', 2.1),
+(24, 2, 12, '2021-11-25', 4.2);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Fruta`
+--
+ALTER TABLE `Fruta`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `FrutasTicket`
+--
+ALTER TABLE `FrutasTicket`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idTicket` (`idTicket`),
+  ADD KEY `idFruta` (`idFruta`);
+
+--
+-- Indices de la tabla `Persona`
+--
+ALTER TABLE `Persona`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `Ticket`
+--
+ALTER TABLE `Ticket`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idPersona` (`idPersona`),
+  ADD KEY `idFrutas` (`idFrutas`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Fruta`
+--
+ALTER TABLE `Fruta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `FrutasTicket`
+--
+ALTER TABLE `FrutasTicket`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `Persona`
+--
+ALTER TABLE `Persona`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `Ticket`
+--
+ALTER TABLE `Ticket`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `FrutasTicket`
+--
+ALTER TABLE `FrutasTicket`
+  ADD CONSTRAINT `fk3` FOREIGN KEY (`idTicket`) REFERENCES `Ticket` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk4` FOREIGN KEY (`idFruta`) REFERENCES `Fruta` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `Ticket`
+--
+ALTER TABLE `Ticket`
+  ADD CONSTRAINT `fk1` FOREIGN KEY (`idPersona`) REFERENCES `Persona` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk2` FOREIGN KEY (`idFrutas`) REFERENCES `FrutasTicket` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- README.md
+++ README.md
@@ -1,9 +1,36 @@
 # Frutería Hidalgo
 Aplicación que se encarga de gestionar una frutería para dar de alta a clientes y poder gestionar en forma de tickets la entrada y salida de cada fruta de la tienda.
 
+## Requerimientos para el despliegue de la app
+
+Se necesita tener instalado **JAVA**, **MYSQL** y un **SERVIDOR WEB** y se recomienda **GIT** en el sistema operativo que se desee usar la aplicación. 
+
+Para usar mysql y un servidor web a la vez, se recomienda instalar [XAMPP](https://www.apachefriends.org/es/download.html), que integra MYSQL y APACHE (servidor web) en una app a la vez. 
+
+Para instalar java se recomienda instalar la última versión en 
+su [web oficial](https://www.java.com/es/download/ie_manual.jsp). 
+
+Se recomienda la instalación de [git](https://git-scm.com/book/es/v2/Inicio---Sobre-el-Control-de-Versiones-Instalaci%C3%B3n-de-Git) para poder actualizar la app más facilmente y descargarsela con el comando:
+```
+git clone https://github.com/tomashm01/Fruteria-Hidalgo.git
+```
+
+## Despliegue de la app 
+
+Una vez instalado todo, se recomienda crearse una carpeta en algun directorio de su disco local y ejecutar el comando para clonar el repositorio(o pegar el contenido de este aquí).
+
+Dentro de la carpeta hay dos archivos que son importantes usar:
+
+- Fruteria_Hidalgo.sql: script que debe ser importado a nuestro gestor de BBDD. Para ello hay que activar previamente XAMPP e importar dicho script en [este enlace](localhost/phpmyadmin).
+- fruteriaHidalgo.jar: ejecutable java que contiene todo el código fuente listo para ejecutar. Para ello nos dirigimos al directorio de este archivo y ejecutamos:
+```
+java -jar fruteriaHidalgo.jar
+```
+
 ## Base de datos
 
 En la siguiente imagen se muestra el diagrama entidad-relación de la base de datos usada en esta aplicación:
+
 ![Modelo entidad-relacional](/src/img/Diagrama.png)
 
 Para ello voy a comentar a continuación acerca de cada entidad creada:
@@ -11,3 +38,13 @@ Para ello voy a comentar a continuación acerca de cada entidad creada:
 * Persona: según el tipo de rol puede ser administrador(puede incluir fruta nueva) o comprador(solo puede retirar fruta)
 * Ticket: ticket generado en una transacción de compra de fruta.
 * FrutasTicket: es una tabla débil, generada principalmente para gestionar la cantidad de frutas que se han comprado en un pedido
+
+## Funcionamiento de la app
+
+La app contiene un menu de identificación de usuario(podemos iniciar sesión o registrarnos en la BBDD).
+
+Una vez registrados, según el rol que tengamos(Administrador o comprador) tendremos acceso a un menu determinado:
+
+- Menu comprador: aquí el usuario solo puede añadir frutas al carrito y mostrar las frutas disponibles con su cantidad y precio.
+
+-Menu administrador: aquí el usuario administrador puede realizar diversas operaciones que no puede realizar el comprador(como crear usuarios administrador, modificar frutas,eliminar usuarios...)
